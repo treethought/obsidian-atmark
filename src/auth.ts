d3af633a0bfc57b4cae0cf9bb944bf4ff38a2fb3
treethought <cpsweene@gmail.com>
2026-01-26 20:42:35 -0800

    pds setting and don't fetch until view activated (#2)

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,21 @@
 import { Client, CredentialManager, simpleFetchHandler } from "@atcute/client";
 
-const DEFAULT_PDS = "https://bsky.social";
+const DEFAULT_SERVICE = "https://bsky.social";
 
 export interface Credentials {
 	identifier: string;
 	password: string;
+	serviceUrl?: string;
 }
 
 export async function createAuthenticatedClient(creds: Credentials): Promise<Client> {
-	const manager = new CredentialManager({ service: DEFAULT_PDS });
+	const service = creds.serviceUrl || DEFAULT_SERVICE;
+	const manager = new CredentialManager({ service });
 	await manager.login(creds);
 	return new Client({ handler: manager });
 }
 
-export function createPublicClient(): Client {
-	return new Client({ handler: simpleFetchHandler({ service: DEFAULT_PDS }) });
+export function createPublicClient(serviceUrl?: string): Client {
+	const service = serviceUrl || DEFAULT_SERVICE;
+	return new Client({ handler: simpleFetchHandler({ service }) });
 }
